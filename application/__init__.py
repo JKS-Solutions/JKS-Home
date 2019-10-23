3a7c6e5f7702e861e7b70e0d1753abdb8b867484
Kim Jämsen <jamsen.kim@gmail.com>
2019-10-23 13:50:17 +0300

    Flask application skeleton, with Heroku compatibility
    ADDED
    > application/* and run.py
       - skeleton for Flask application with Heroku support, contains SQLAlchemy and PosrgreSQL
       - requirements.txt -> dependencies.
    > Herokuthings
       - Procfile
    MODIFIED
    > .gitignore
        -added ignores for vs-code.

---
+++ application/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+import os
+
+# database
+from flask_sqlalchemy import SQLAlchemy
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+
+#application
+from application import views
+
+# create tables
+try: 
+    db.create_all()
+except:
+    pass
